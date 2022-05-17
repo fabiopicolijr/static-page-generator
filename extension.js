07c3cae458b54aee56c3ae2f15eed8d4fd89f024
Fábio Picoli <fabiopicolijr@gmail.com>
2022-05-17 19:59:17 -0300

    improve code and staticEnd import

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 const vscode = require('vscode');
 const fs = require('fs');
 
+const workspaceRootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -12,136 +14,28 @@ function activate(context) {
   let disposable = vscode.commands.registerCommand(
     'static-page-generator.generateStaticPage',
     function () {
-      const workspaceRootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-      const indexFile = `${workspaceRootPath}/src/index.html`;
-      const staticBeforeFile = `${workspaceRootPath}/src/staticBefore.txt`;
-      const distPath = `${workspaceRootPath}/dist`;
-      const bundleFile = `${distPath}/bundle.htm`;
-      const tmpPath = `${distPath}/tmp`;
-      const indexTmpFile = `${tmpPath}/index.html`;
-      //const newLine = '\n';
+      const files = {
+        indexFile: `${workspaceRootPath}\\src\\index.html`,
+        gspPath: `${workspaceRootPath}\\gsp`,
+        staticBegin: `${workspaceRootPath}\\gsp\\static\\indexBegin.html`,
+        staticEnd: `${workspaceRootPath}\\gsp\\static\\indexEnd.html`,
+        distPath: `${workspaceRootPath}\\gsp\\dist`,
+        bundle: `${workspaceRootPath}\\gsp\\dist\\bundle.htm`,
+        tmpPath: `${workspaceRootPath}\\gsp\\dist\\tmp`,
+        indexTmpFile: `${workspaceRootPath}\\gsp\\dist\\tmp\\index.html`,
+      };
 
       try {
-        console.log('LOG: try');
-        // CHECK INDEX FILE
-        if (!fs.existsSync(indexFile))
-          throw new Error('src/index.html does not exists.');
+        createFiles(files);
+        appendDataFile(files.staticBegin, files.bundle);
+        importCssJs(filterBundle(files), files);
+        appendDataFile(files.staticEnd, files.bundle);
 
-        // CREATE DIST FOLDER
-        if (!fs.existsSync(distPath)) fs.mkdirSync(distPath);
-
-        // CREATE TMP FOLDER
-        if (!fs.existsSync(tmpPath)) fs.mkdirSync(tmpPath);
-
-        // EMPTY TMP/INDEX FILE
-        if (fs.existsSync(indexTmpFile)) {
-          fs.truncate(indexTmpFile, 0, function () {
-            // COPY INDEX FILE TO DIST/TMP
-            fs.copyFileSync(indexFile, indexTmpFile);
-          });
-        } else {
-          // COPY INDEX FILE TO DIST/TMP
-          fs.copyFileSync(indexFile, indexTmpFile);
-        }
-
-        // EMPTY BUNDLE FILE
-        if (fs.existsSync(bundleFile))
-          fs.truncate(bundleFile, 0, function () {});
-
-        // [OPCIONAL] GET STATIC CONTENT: before HTML TAG
-        if (fs.existsSync(staticBeforeFile)) {
-          //prettier-ignore
-          const staticBeforeData = fs.readFileSync(staticBeforeFile, {encoding: 'utf8', flag: 'r'});
-
-          //prettier-ignore
-          fs.appendFileSync(bundleFile, staticBeforeData + '\r\n');
-          // fs.appendFile(bundleFile, staticBeforeData, (err) => {
-          //   if (err) throw err;
-          // });
-        }
-
-        console.log('LOG: try END');
-        // GET HTML CONTENT
-
-        // GENERATE BUNDLE FILE.HTM
         //prettier-ignore
+        vscode.window.showInformationMessage(`File generated at ${files.bundle}`);
       } catch (err) {
-        vscode.window.showErrorMessage('ERROR: ' + err.message);
-        return null;
+        vscode.window.showErrorMessage('ERROR: ' + err);
       }
-
-      // FILTER HTML DATA
-      const indexTmpData = fs.readFileSync(indexTmpFile, 'utf-8').split('\r\n');
-
-      const ignoredLines = [
-        '.min.js',
-        '.min.css',
-        '.mask.js',
-        '<!DOCTYPE html>',
-        'crossorigin="anonymous"',
-        'prettier-ignore',
-      ];
-
-      const indexTmpDataFiltered = indexTmpData.filter(function (line) {
-        return !ignoredLines.some((el) => line.includes(el));
-      });
-
-      // IMPORT CSS AND JS FILES INTO HTML DATA
-      //prettier-ignore
-      let firstJsFile = true;
-
-      console.log('indexTmpDataFiltered.length', indexTmpDataFiltered.length);
-
-      // prettier-ignore
-      const indexTmpDataImported = indexTmpDataFiltered.map(function (line) {                
-
-        if (line.includes('stylesheet') && line.includes('.css')) {
-          const cssFile = `${workspaceRootPath}/src/${matchHref(line)}`;
-
-          if (!fs.existsSync(cssFile)) {
-            return `${cssFile} não encontrado`;
-          }
-
-          const cssData = fs.readFileSync(cssFile, {
-            encoding: 'utf8',
-            flag: 'r',
-          });
-
-          return `<style type="text/css">\r\n${cssData}</style>`;
-        }
-
-        if (line.includes('script') && line.includes('.js')) {
-          const jsFile = `${workspaceRootPath}/src/${matchSrc(line)}`;
-
-          if (!fs.existsSync(jsFile)) {
-            return `${jsFile} não encontrado`;
-          }
-
-          const jsData = fs.readFileSync(jsFile, {
-            encoding: 'utf8',
-            flag: 'r',
-          });
-
-          if (firstJsFile) {
-            firstJsFile = false;
-            return `<script type="text/javascript">\r\n${jsData}`;
-          }
-
-          return jsData;
-        }
-
-        return line.trim() === '</body>'
-          ? `</script>\r\n</body>`
-          : line;
-      });
-
-      // APPEND DATA
-      const indexTmpDataImportedString = indexTmpDataImported.join('\r\n');
-
-      fs.appendFileSync(bundleFile, indexTmpDataImportedString);
-
-      console.log('LOG: File generated at dist/bundle.htm');
-      vscode.window.showInformationMessage('File generated at dist/bundle.htm');
     }
   );
 
@@ -156,6 +50,120 @@ module.exports = {
   deactivate,
 };
 
+function createFiles(files) {
+  console.log('createFiles Begin');
+
+  if (!fs.existsSync(files.indexFile))
+    throw new Error('src/index.html does not exists.');
+
+  if (!fs.existsSync(files.gspPath)) fs.mkdirSync(files.gspPath);
+  if (!fs.existsSync(files.distPath)) fs.mkdirSync(files.distPath);
+  if (!fs.existsSync(files.tmpPath)) fs.mkdirSync(files.tmpPath);
+
+  fs.copyFileSync(files.indexFile, files.indexTmpFile);
+  if (fs.existsSync(files.bundle)) fs.truncateSync(files.bundle, 0);
+
+  console.log('createFiles End');
+}
+
+function appendDataFile(file, destinationFile) {
+  const line = '\r\n';
+  const lineBefore = !file.includes('indexBegin.html') ? line : '';
+
+  if (fs.existsSync(file)) {
+    const staticData = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+
+    fs.appendFileSync(destinationFile, lineBefore + staticData + line);
+  }
+
+  console.log('appendData End');
+}
+
+function filterBundle(files) {
+  console.log('filterIndexData Begin');
+  // FILTER HTML DATA
+  const indexTmpData = fs
+    .readFileSync(files.indexTmpFile, 'utf-8')
+    .split('\r\n');
+
+  const ignoredLines = [
+    '.min.js',
+    '.min.css',
+    '.mask.js',
+    '<!DOCTYPE html>',
+    'crossorigin="anonymous"',
+    'prettier-ignore',
+    'spg-ignore',
+  ];
+
+  const indexTmpDataFiltered = indexTmpData.filter(function (line) {
+    return !ignoredLines.some((el) => line.includes(el));
+  });
+
+  console.log('filterIndexData End');
+
+  return indexTmpDataFiltered;
+}
+
+function importCssJs(indexTmpDataFiltered, files) {
+  console.log('importCssJs Begin');
+  // IMPORT CSS AND JS FILES INTO HTML DATA
+  //prettier-ignore
+  let firstJsFile = true;
+
+  console.log(indexTmpDataFiltered);
+
+  // prettier-ignore
+  const indexTmpDataImported = indexTmpDataFiltered.map(function (line) {                
+
+    if (line.includes('stylesheet') && line.includes('.css')) {
+      const cssFile = `${workspaceRootPath}/src/${matchHref(line)}`;
+
+      if (!fs.existsSync(cssFile)) {
+        return `${cssFile} não encontrado`;
+      }
+
+      const cssData = fs.readFileSync(cssFile, {
+        encoding: 'utf8',
+        flag: 'r',
+      });
+
+      return `<style type="text/css">\r\n${cssData}</style>`;
+    }
+
+    if (line.includes('script') && line.includes('.js')) {
+      const jsFile = `${workspaceRootPath}/src/${matchSrc(line)}`;
+
+      if (!fs.existsSync(jsFile)) {
+        return `${jsFile} não encontrado`;
+      }
+
+      const jsData = fs.readFileSync(jsFile, {
+        encoding: 'utf8',
+        flag: 'r',
+      });
+
+      if (firstJsFile) {
+        firstJsFile = false;
+        return `<script type="text/javascript">\r\n${jsData}`;
+      }
+
+      return jsData;
+    }
+
+    return line.trim() === '</body>'
+      ? `</script>\r\n</body>`
+      : line;
+  });
+
+  const indexTmpDataImportedString = indexTmpDataImported.join('\r\n');
+
+  fs.appendFileSync(files.bundle, indexTmpDataImportedString);
+
+  console.log('importCssJs End');
+}
+
+// Utils.js
 function matchHref(string) {
   return JSON.stringify(string.match(/href="([^\'\"]+)/g))
     .replace(/[\]\="\)}[{(]/g, '')
